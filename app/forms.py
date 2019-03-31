87a0ad532f7ec8a13275d5c56e7d4c829739c20f
Rushane Barnes <barnesrushane@hotmail.com>
2019-03-31 17:00:59 +0000

    Exercise 1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,11 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, PasswordField, TextAreaField, TextField, SelectField, validators
+from wtforms.validators import InputRequired, Email, DataRequired
 
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description', validators=[DataRequired()])
+    photo = FileField('Photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png', 'Images only!'])
+    ])
